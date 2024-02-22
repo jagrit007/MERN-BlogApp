3d7cb6099540596a1412b098336b1f714bc2c2bd
Jagrit Thapar <jagritthapar007@gmail.com>
2024-02-22 18:37:41 +0530

    Add pages, navbar component and misc - Added comments in App.js - Added Navbar.js - Added useParams - The added pages are dummy pages at this point

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Pages
 import { Home } from './pages/Home';
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticlesList from './pages/ArticlesList';
+
+// Components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar/>
     <div className='max-w-screen-md mx-auto pt-20'>
-      {/* to navigate pages -> react-router-dom */}
       <Routes>
           <Route path='/' element={<Home/>} ></Route>
+          <Route path='/about' element={<About/>} ></Route>
+          <Route path='/article/:name' element={<Article/>} ></Route>
+          <Route path='/article-list' element={<ArticlesList/>} ></Route>
       </Routes>
     </div>
     </Router>
   );
 }
+
+// Comments:
+/*  1. To navigate pages -> react-router-dom 
+    2. To pass parameters in the Router, i.e browser url `site-url/article/my-first-article` 
+       - Use `param` in the Route path like this: `<Route path='/article /:id' element={<Article />}></Route>`
+       we use :paramName . In this case it will be :slug
+*/
 
 export default App;
---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticlesList = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-600'>Articles</h1>
+    </div>
+  )
+}
+
+export default ArticlesList
