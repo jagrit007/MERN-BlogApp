15d6d438d8998c01bd7d3c70b33d5e56fb93d87f
Jagrit Thapar <jagritthapar007@gmail.com>
2024-02-22 18:09:09 +0530

    Initial with Tailwind, Routes and Simple Homescreen

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-600'>Welcome to our Blog</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Amet magna labore culpa occaecat ad cillum laboris sunt commodo enim. Lorem sit mollit eiusmod officia duis labore laboris. Pariatur cupidatat aliquip sint minim ipsum cillum aute. Id proident culpa proident occaecat laborum pariatur excepteur culpa id et duis. Do amet duis minim proident excepteur. Nisi mollit nisi aliquip eiusmod enim exercitation dolore. Amet sint occaecat non reprehenderit eiusmod consectetur pariatur incididunt esse elit cillum.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Culpa aliquip incididunt laborum cupidatat fugiat ipsum irure minim qui dolore nostrud dolore pariatur. Sit nostrud cillum amet officia qui consectetur nisi laboris ipsum sit reprehenderit pariatur enim. Ea aliquip voluptate nulla sint irure esse et ipsum sunt occaecat officia. Nulla deserunt adipisicing sit proident incididunt quis consectetur minim veniam irure sit. Magna ipsum et dolore aute ipsum proident consectetur. Labore consequat anim in Lorem veniam minim adipisicing ut est magna veniam ex incididunt.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Anim labore esse sunt qui. Anim in laborum est laboris eiusmod pariatur enim. Cillum duis labore aute labore. Ad ut duis minim et quis sunt aliqua. Ad amet excepteur irure est velit laborum.</p>
+    </div>
+  )
+}
